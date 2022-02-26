929fb5c0314eac7a2086f88f7150e1d3ccc1921e
pxblo14 <pxblo14@gmail.com>
2022-02-25 23:30:52 -0300

    Correcion de Header y nombre

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Apple Libertador San Martin';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Apple Libertador San Martin';
+    : 'Novedades';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'Instagram: apple.libertador';
