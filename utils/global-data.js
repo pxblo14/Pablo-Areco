a406ca292fdcdaf7b3c998173b9237e5a85379da
pxblo14@gmail.com <pxblo14@gmail.com>
2024-01-15 17:09:42 -0300

    Se modificaron los datos principales. Como el nombre, titulo y footer

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Apple Libertador San Martin';
+    : 'Curriculum Vitae';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Novedades';
+    : 'Pablo Areco';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'Instagram: apple.libertador';
+    : 'Email: pxbloart@gmail.com';
 
   return {
     name,
